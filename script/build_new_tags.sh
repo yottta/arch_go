62d461ccf42fd7da8dd90bea3e2b86f6965624ff
andreic92 <andreic9203@gmail.com>
2019-03-07 13:50:25 +0200

    fixed env variables; cleanup

--- script/build_new_tags.sh
+++ script/build_new_tags.sh
@@ -1,15 +1,13 @@
 #!/bin/bash
-
 set -e
 
 script_path=$(cd `dirname $0` && pwd)
-echo $CIRCLECI_ENV_IN_SCRIPT
+
 for tag in $(git ls-remote --tags https://go.googlesource.com/go | awk '{print $2}' | grep refs/tags/go | egrep -v "go1\.[0-1]{1}\..*$" | egrep -v "go1\.1$" | egrep -v "go1$" | cut -d'/' -f3); do
   echo "Build image $tag"
   docker build $script_path/../docker --build-arg go_version=$tag -t arch-go
 
-	docker login -u "$DOCKER_HUB_USER_ID" -p "$DOCKER_HUB_PWD"
-	#docker login -e $DOCKER_HUB_EMAIL -u $DOCKER_HUB_USER_ID -p $DOCKER_HUB_PWD
+	docker login -u "$DOCKER_HUB_USER_ID" -p "$DOCKER_HUB_USER_PWD"
 	docker tag arch-go $DOCKER_HUB_USER_ID/arch-go:$tag
 	docker tag arch-go $DOCKER_HUB_USER_ID/arch-go:latest
 	docker push $DOCKER_HUB_USER_ID/arch-go:$tag
