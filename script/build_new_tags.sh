c1c18717e299cfb7b5ddaba5ace202f4bf70ed5b
andreic92 <andreic9203@gmail.com>
2019-03-08 07:07:17 +0200

    add final messages

--- script/build_new_tags.sh
+++ script/build_new_tags.sh
@@ -17,6 +17,8 @@ existing_tags=" $(wget -q https://registry.hub.docker.com/v1/repositories/$DOCKE
 
 docker login -u "$DOCKER_HUB_USER_ID" -p "$DOCKER_HUB_USER_PWD"
 
+failed_tags=""
+skipped_tags=""
 
 for tag in $(git ls-remote --tags https://go.googlesource.com/go | awk '{print $2}' | grep refs/tags/go | egrep -v "go1\.[0-1]{1}\..*$" | egrep -v "go1\.1$" | egrep -v "go1$" | cut -d'/' -f3); do
   print_message "Build image $tag"
@@ -26,6 +28,7 @@ for tag in $(git ls-remote --tags https://go.googlesource.com/go | awk '{print $
   result=$?
   if [ $result -eq  0 ]; then
     print_message "$tag already exists. Skipping."
+    skipped_tags="$skipped_tags $tag"
     continue
   fi
   docker build $script_path/../docker --build-arg go_version=$tag -t arch-go
@@ -33,6 +36,7 @@ for tag in $(git ls-remote --tags https://go.googlesource.com/go | awk '{print $
   set -e
   if [ $result -ne  0 ]; then
     print_message "$tag failed building the image. Skipped."
+    failed_tags="$failed_tags $tag"
     continue
   fi
 
@@ -42,3 +46,6 @@ for tag in $(git ls-remote --tags https://go.googlesource.com/go | awk '{print $
 
 	print_message "$tag image processed successfully"
 done
+
+print_message "Skipped tags: $skipped_tags"
+print_message "Failed tags: $failed_tags"
