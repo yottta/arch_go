6d97803aadf2d91a2f21dee3540f370df87ce308
yottta <andreic9203@gmail.com>
2022-09-19 16:23:55 +0300

    fix(cicd): rework the build script to exit on error

--- build_new_tags.sh
+++ build_new_tags.sh
@@ -13,34 +13,24 @@ function print_message() {
 
 script_path=$(cd `dirname $0` && pwd)
 
-existing_tags=" $(wget -q https://registry.hub.docker.com/v1/repositories/$DOCKER_HUB_USER_ID/arch-go/tags -O -  | sed -e 's/[][]//g' -e 's/"//g' -e 's/ //g' | tr '}' '\n'  | awk -F: '{print $3}') "
-
+existing_tags="$(wget -q https://hub.docker.com/v2/namespaces/$DOCKER_HUB_USER_ID/repositories/arch-go/tags -O - | jq '.results[].name')"
+echo "$existing_tags"
 docker login -u "$DOCKER_HUB_USER_ID" -p "$DOCKER_HUB_USER_PWD"
 
-failed_tags=""
 skipped_tags=""
 built_tags=""
 
 for tag in $(git ls-remote --tags https://go.googlesource.com/go | awk '{print $2}' | grep refs/tags/go | egrep -v "go1\.[0-1]{1}\..*$" | egrep -v "go1\.1$" | egrep -v "go1$" | cut -d'/' -f3); do
     print_message "Build image $tag"
 
-    set +e
-    echo "#start#"${existing_tags}"#end#" | grep "\s$tag\s"> /dev/null
-    result=$?
-    if [ $result -eq  0 ]; then
+    res=`echo "${existing_tags}" | grep '"'$tag'"' || true`
+    if [ ! -z "$res" ]; then
         print_message "$tag already exists. Skipping."
         skipped_tags="$skipped_tags $tag"
         continue
     fi
-    docker build $script_path/docker --build-arg GO_VERSION=$tag -t arch-go
-    result=$?
-    set -e
-    if [ $result -ne  0 ]; then
-        print_message "$tag failed building the image. Skipped."
-        failed_tags="$failed_tags $tag"
-        continue
-    fi
 
+    docker build --platform linux/amd64 $script_path/docker --build-arg GO_VERSION=$tag -t arch-go
     docker tag arch-go $DOCKER_HUB_USER_ID/arch-go:$tag
     docker tag arch-go $DOCKER_HUB_USER_ID/arch-go:latest
     docker push $DOCKER_HUB_USER_ID/arch-go:$tag
@@ -50,5 +40,4 @@ for tag in $(git ls-remote --tags https://go.googlesource.com/go | awk '{print $
 done
 
 print_message "Skipped tags: $skipped_tags"
-print_message "Failed tags: $failed_tags"
 print_message "Successfully built tags: $built_tags"
