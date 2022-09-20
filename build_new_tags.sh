62af03024d781e167d660e74d63b5955fb8a6839
yottta <andreic9203@gmail.com>
2022-09-20 16:52:41 +0300

    fix(script): docker get all tags from all pages

--- build_new_tags.sh
+++ build_new_tags.sh
@@ -13,8 +13,19 @@ function print_message() {
 
 script_path=$(cd `dirname $0` && pwd)
 
-existing_tags="$(wget -q https://hub.docker.com/v2/namespaces/$DOCKER_HUB_USER_ID/repositories/arch-go/tags?page_size=1000 -O - | jq '.results[].name')"
-echo "$existing_tags"
+existing_tags=""
+docker_tags_url="https://hub.docker.com/v2/namespaces/$DOCKER_HUB_USER_ID/repositories/arch-go/tags?page_size=1000"
+while true; do
+    res="$(wget -q $docker_tags_url -O -)"
+    existing_tags="$existing_tags $(echo $res | jq '.results[].name') \n"
+    docker_tags_url="$(echo $res | jq -r '.next')"
+    if [ "$docker_tags_url" = "null" ]; then
+        break
+    fi
+    echo "get the next page $docker_tags_url"
+done
+echo "existing tags"
+echo -e "$existing_tags"
 docker login -u "$DOCKER_HUB_USER_ID" -p "$DOCKER_HUB_USER_PWD"
 
 skipped_tags=""
@@ -23,7 +34,7 @@ built_tags=""
 for tag in $(git ls-remote --tags https://go.googlesource.com/go | awk '{print $2}' | grep refs/tags/go | egrep -v "go1\.[0-2]{1}(\..*)?$" | egrep -v "go1\.1$" | egrep -v "go1$" | cut -d'/' -f3); do
     print_message "Build image $tag"
 
-    res=`echo "${existing_tags}" | grep '"'$tag'"' || true`
+    res=`echo -e "${existing_tags}" | grep '"'$tag'"' || true`
     if [ ! -z "$res" ]; then
         print_message "$tag already exists. Skipping."
         skipped_tags="$skipped_tags $tag"
