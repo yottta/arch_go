4a7c0286e1076952e80cd10c865711de5e811ffe
yottta <andreic9203@gmail.com>
2022-09-22 00:15:14 +0300

    fix(script): get all go tags but skip the ones that does not have a download solution

--- build_new_tags.sh
+++ build_new_tags.sh
@@ -30,9 +30,10 @@ docker login -u "$DOCKER_HUB_USER_ID" -p "$DOCKER_HUB_USER_PWD"
 
 skipped_tags=""
 built_tags=""
+failed_tags=""
 
-for tag in $(git ls-remote --tags https://go.googlesource.com/go | awk '{print $2}' | grep refs/tags/go | egrep -v "go1\.[0-2]{1}(\..*)?$" | egrep -v "go1\.1$" | egrep -v "go1$" | cut -d'/' -f3); do
-    print_message "Build image $tag"
+for tag in $(git ls-remote --tags https://go.googlesource.com/go | awk '{print $2}' | grep refs/tags/go | cut -d'/' -f3); do
+    print_message "Build image for go tag $tag"
 
     res=`echo -e "${existing_tags}" | grep '"'$tag'"' || true`
     if [ ! -z "$res" ]; then
@@ -41,7 +42,14 @@ for tag in $(git ls-remote --tags https://go.googlesource.com/go | awk '{print $
         continue
     fi
 
+    set +e
     docker build --platform linux/amd64 $script_path/docker --build-arg GO_VERSION=$tag -t arch-go
+    if [ $? -ne 0 ]; then
+        failed_tags="$failed_tags $tag"
+        print_message "Build image failed for go tag $tag"
+        continue
+    fi
+    set -e
     docker tag arch-go $DOCKER_HUB_USER_ID/arch-go:$tag
     docker tag arch-go $DOCKER_HUB_USER_ID/arch-go:latest
     docker push $DOCKER_HUB_USER_ID/arch-go:$tag
@@ -52,3 +60,4 @@ done
 
 print_message "Skipped tags: $skipped_tags"
 print_message "Successfully built tags: $built_tags"
+print_message "Failed tags: $failed_tags"
